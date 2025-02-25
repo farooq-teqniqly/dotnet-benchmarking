72c4d77c40e60d2b08b25dfec2389cf68a0af2a2
farooq-teqniqly <farooq@teqniqly.com>
2025-02-24 16:06:44 -0800

    wip

---
+++ EfCoreBenchmarks/EfCoreBenchmarksApp/Models.cs
@@ -0,0 +1,15 @@
+﻿namespace EfCoreBenchmarksApp
+{
+    public class Order
+    {
+        public DateTime OrderDate { get; set; }
+        public string OrderId { get; set; } = default!;
+        public decimal TotalAmount { get; set; }
+    }
+
+    public class User
+    {
+        public ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+        public string UserId { get; set; } = default!;
+    }
+}
---
+++ EfCoreBenchmarks/EfCoreBenchmarksApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Testcontainers.MsSql;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets(Assembly.GetExecutingAssembly())
+    .Build();
+
+await ConfigureAndStartContainer(config);
+Console.WriteLine("Hello, World!");
+return;
+
+string GetContainerName(string prefix = "benchmark-efcore") => $"{prefix}-{Ulid.NewUlid().ToString()[..10]}".ToLowerInvariant();
+
+async Task ConfigureAndStartContainer(IConfigurationRoot config)
+{
+    var dbPassword = config["DatabaseSettings:Password"] ?? throw new InvalidOperationException("Database password not set.");
+    var containerName = GetContainerName();
+
+    var sqlContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+        .WithPassword(dbPassword)
+        .WithPortBinding(1433, 1433)
+        .WithName(containerName)
+        .Build();
+
+    await sqlContainer.StartAsync();
+}
